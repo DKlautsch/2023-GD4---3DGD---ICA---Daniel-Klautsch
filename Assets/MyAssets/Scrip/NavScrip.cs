fe14586c81de51157dd4615e28956b29c2ce5a6f
DKlautsch <113542526+DKlautsch@users.noreply.github.com>
2023-12-24 13:36:35 +0000

    :walking: : Added PlayerNavigation
    He can walk, he can talk.

---
+++ Assets/MyAssets/Scrip/NavScrip.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NavScrip : MonoBehaviour
+{
+    public UnityEvent onAnswerGiven;
+
+    public bool HasKey{
+        get {return hasKey;}
+        set {hasKey = value;}
+    }
+
+    private bool hasKey;
+
+
+
+    [SerializeField]
+    private UnityEngine.AI.NavMeshAgent player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray target = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(target, out var hitInfo))
+            {
+                player.SetDestination(hitInfo.point);
+            }
+
+
+
+        }
+    }
+
+    public void ReSet()
+    {
+        this.transform.position = new Vector3(0.0f,0.0f,0.0f);
+        hasKey = false;
+        player.SetDestination(this.transform.position);
+    }
+
+}
